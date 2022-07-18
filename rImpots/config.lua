66295321fbf8343a5a3e7ab25a169c0ae3867fce
Rayan Waize <88369314+RayanWaize@users.noreply.github.com>
2022-07-19 02:31:05 +0300

    Update config.lua

--- rImpots/config.lua
+++ rImpots/config.lua
@@ -1,5 +1,5 @@
-local second = 1000
-local minutes = 60 * second
+local second = 1000 -- Faut pas toucher
+local minutes = 60 * second -- Faut pas toucher
 
 Config = {
     timeSampling = 60 * minutes, -- 1H
