5066f013b2576b003cb16bcb941e5f805ea80310
Rayan Waize <88369314+RayanWaize@users.noreply.github.com>
2022-07-19 02:27:06 +0300

    Update config.lua

--- rImpots/config.lua
+++ rImpots/config.lua
@@ -2,7 +2,7 @@ local second = 1000
 local minutes = 60 * second
 
 Config = {
-    timeSampling = 1 * minutes, -- 1H
+    timeSampling = 60 * minutes, -- 1H
     textNotification = "Vous avez été prélevé de ~r~%s$~s~ d'impôts",
     pourcentageImpots = 10, -- Prend 10%
     accountName = 'bank',
